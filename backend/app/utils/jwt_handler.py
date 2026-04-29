70de8e7baa4352ec9c45bb9a5adfd58643244cbb
reevacodes <rg6596070@gmail.com>
2026-04-29 16:37:32 +0530

    fix: add default values for jwt env vars

--- backend/app/utils/jwt_handler.py
+++ backend/app/utils/jwt_handler.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
 print("SECRET_KEY:", SECRET_KEY)
 print("ALGORITHM:", ALGORITHM)
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "600"))
 
 def create_access_token(data: dict):
     to_encode = data.copy()
